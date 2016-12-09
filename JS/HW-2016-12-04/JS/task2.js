21dab66759a790cd20b52342afebc714a7024ee2
e0257 <yaroslav.bushmakin@gmail.com>
2016-12-10 01:05:49 +0200

    update task with API, fail task2,js

--- JS/HW-2016-12-04/JS/task2.js
+++ JS/HW-2016-12-04/JS/task2.js
@@ -15,74 +15,62 @@ while (loop) {
 let api = fetch("http://swapi.co/api/people/" + answer + "/");
 
 let result;
-let p1,p2,p3;//промисы
 
 let prom = api.then(function(res){
 	return res.json();
 })
 .then(function(person){
-	let name, films = [], species = [], language = [], same = [];
-	console.log(person);
 	//name
-	name = person.name;
+	let name = Promise.resolve(person.name);
+	
+	let getFilms = person.films.map(url => fetch(url));
+	let getSpecies = person.species.map(url => fetch(url));
 
 	//films
-	person.films.forEach( e => {
-		p1 = fetch(e).then(function(res){
-			return res.json();
-		})
-		.then(function(film){
-			films.push(film.title);
-		});
-
-	});
-
-	//species, language, same origin
-	person.species.forEach( e => {
-		p2 = fetch(e).then(function(res){
-			return res.json();
-		})
-		.then(function(spec){
-			species.push(spec.name);
-			language.push(spec.language);
-			spec.people.forEach( e => {
-				p3 = fetch(e).then(function(res){
-					return res.json();
-				})
-				.then(function(people){
-					same.push(people.name);
-				});
+	let films = Promise.all(getFilms)
+			.then( res => {
+				let  films = res.map(res => res.json());
+				return Promise.all(films);
+			}).then (res => {
+				let result = res.map(el => el.title);
+				return Promise.all(result);
 			});
 
-		});
-	});
+	//species, language, same origin
+	let species = Promise.all(getSpecies)
+			.then( res => {
+				let  species = res.map(res => res.json());
+				return Promise.all(species);
+			}).then (res => {
+				let name = res.map(el => el.name);
+				let language = res.map(el => el.language);
+				let getPeople = res.map(res => {
+					let getPeople = res.people.map(url => fetch(url));
 
-			result = {
-				name: name, 
-				films: films, 
-				species: species, 
-				language: language,
-				same: same
-			};
-	
+					return  Promise.all(getPeople)
+					.then(res => {
+						let  people = res.map(res => res.json());
+						return Promise.all(people);
+					}).then(res => {
+						 let people = res.map(el => el.name);
+						return Promise.all(people);
+					})
+				})
+			return Promise.all([name, language].concat(getPeople))
+				.then(res => ({spec: res[0], lang: res[1], same: res[2]}));
+			});
+
+
+	return Promise.all([name, films, species])
+		.then(res => ({name: res[0], films: res[1], species: res[2].spec, language: res[2].lang, same: res[2].same}));
+
 })
-.catch(function(rej){
-	console.log(rej);
-});
-
-
-Promise.all([prom, p1, p2, p3]).then(function(res){
-		//console.log(result);
-});
-
-setTimeout( 
-function(){
+.then(result => {
 console.log(`Name: ${result.name} 
 Films: ${result.films}
 Species: ${result.species}
 Language: ${result.language}
 Same Origin: ${result.same}
 `);
-}, 5000);
-
+});
 
